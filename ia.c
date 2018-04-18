1ab9678cc589d71587d669213e966989998fa5be
ihfisk <ihfisk42@gmail.com>
2018-04-18 15:46:41 +0200

    mzfe

--- ia.c
+++ ia.c
@@ -9,14 +9,14 @@ void ia_aleatoire(plateau p)
 	int x = rand() % 8;
 	int y = rand() % 8;
 
-	// On retente un coup aléatoire tant qu'il n'est pas valide
+	/* On retente un coup aléatoire tant qu'il n'est pas valide */
 	while(!coup_valide(p,x,y,BLANC))
 	{
 		x = rand() % 8;
 		y = rand() % 8;
 	}
 
-	// puis on capture les pions une fois qu'on est bon
+	/* puis on capture les pions une fois qu'on est bon */
 	capture(p,x,y,BLANC);
 }
 
